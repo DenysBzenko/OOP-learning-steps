ef4718780a789ad9d9272664130734341d2102d0
Denys Bzenko <dbzenko@kse.org.ua>
2023-12-02 15:33:30 +0200

    CREARE CLASS FROENDSHIP AND TESTER FOR CLASSES

--- Testproject/main.cpp
+++ Testproject/main.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<string>
-#include <vector>
+#include<vector>
 
 using namespace std;
 
@@ -11,12 +11,9 @@ private:
     string Name;
     string Surnname;
 
-    // TODO
-    // Define public methods to manipulate our data
 public:
-
     Human(const float& height, const string& Name, const string& Surnname) : height(height), Name(Name), Surnname(Surnname) {
-    };
+    }
 
     float getHeight() const {
         return height;
@@ -29,21 +26,24 @@ public:
     const string& getSurname() const {
         return Surnname;
     }
-
 };
-//TOD
-// create a class family
-class Family {
+
+class Family
+{
 private:
     Human Husband;
     Human Wife;
-    vector<Human> child;
+    vector<Human> children;
+
 public:
+    Family(const Human& Husband, const Human& Wife) : Husband(Husband), Wife(Wife) {
+    }
 
-    
-    Family(const Human& Husband, const Human& Wife, const vector<Human>& children) : Husband(Husband), Wife(Wife), child(child) {}
+    void addChild(const Human& child) {
+        children.push_back(child);
+    }
 
-    void UserInterface() const{
+    void UserInterface() const {
         cout << "Husband:" << endl;
         cout << "Height:" << Husband.getHeight() << endl;
         cout << "Name:" << Husband.getName() << endl;
@@ -53,19 +53,57 @@ public:
         cout << "Name:" << Wife.getName() << endl;
         cout << "Surnname:" << Wife.getSurname() << endl;
 
-        
-        cout << "Name of our Parants:" << endl;
-        cout << "MOM:\n" << Wife.getName() << endl;
-        cout << "PAPA:\n" << Husband.getName() << endl;
+        cout << "Children:" << endl;
+        for (const auto& child : children) {
+            cout << "Height:" << child.getHeight() << endl;
+            cout << "Name:" << child.getName() << endl;
+            cout << "Surname:" << child.getSurname() << endl;
+        }
+    }
+};
+
+class Friendship {
+private:
+    Family& family1;
+    Family& family2;
+
+public:
+    Friendship(Family& family1, Family& family2) : family1(family1), family2(family2) {
     }
 
-
-   
+    void infoFriends() const {
+        cout << "Friends Family:\n";
+        cout << "First friends family:\n";
+        family1.UserInterface();
+        cout << "Second friends family:\n";
+        family2.UserInterface();
+        
+    }
 };
+
+class FriendsTester {
+public:
+    static void testFriends() {
+        Human Man1(80.0, "DAN", "BOLAN");
+        Human Woman1(75.0, "KRIS", "BOLAN");
+
+        Human Man2(85.0, "IVAN", "CRASH");
+        Human Woman2(78.0, "NATA", "CRASH");
+
+        Family family1(Man1, Woman1);
+        Family family2(Man2, Woman2);
+
+        Friendship friendship(family1, family2);
+
+        friendship.infoFriends();
+    }
+};
+
+
+
 class FamilyTester {
 public:
     static void testFamily() {
-        
         Human Man(86.8, "ALEX", "DAO");
         Human Woman(43.6, "Alice", "DAO");
 
@@ -73,28 +111,32 @@ public:
         cout << "Enter the number of children: ";
         cin >> number;
 
-        
         vector<Human> child;
         for (int i = 0; i < number; ++i) {
             float height;
             string Name, Surnname;
-            cout << "enter Height:\n " << i + 1 << ":";
+            cout << "enter Height for child " << i + 1 << ": ";
             cin >> height;
-            cout << "enter Name:\n" << i + 1 << ":";
+            cout << "enter Name for child " << i + 1 << ": ";
             cin >> Name;
-            cout << "enter surname:\n " << i + 1 << ":";
+            cout << "enter surname for child " << i + 1 << ": ";
             cin >> Surnname;
             child.push_back(Human(height, Name, Surnname));
         }
+        Family myFamily(Man, Woman);
+        for (const auto& childs : child) {
+            myFamily.addChild(childs);
+        }
 
-        Family myFamily(Man, Woman, child);
         myFamily.UserInterface();
     }
+   
 };
 
 int main() {
-
+    
     FamilyTester::testFamily();
-    return 0;
+    FriendsTester::testFriends();
 
-};
+    return 0;
+}
